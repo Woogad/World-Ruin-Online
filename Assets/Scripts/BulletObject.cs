9fd1be387de0fb9dbbf5817b21f4e133e1af7f89
Teerasak Jumwanta <vgthailand@hotmail.com>
2023-05-15 16:30:31 +0700

    add Assets , BulletSpread

--- Assets/Scripts/BulletObject.cs
+++ Assets/Scripts/BulletObject.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletObject : MonoBehaviour
 {
     private float _damage;
 
-    public void Setup(Transform shootDir, ShootConfigSO shootConfigOS)
+    public void Setup(Transform FireEndPoint, ShootConfigSO shootConfigOS)
     {
         this._damage = shootConfigOS.Damage;
         Rigidbody rigidbody = GetComponent<Rigidbody>();
 
-        float _moveSpeed = 60f;
-        rigidbody.AddForce(shootDir.forward * _moveSpeed, ForceMode.Impulse);
+        //* Random Shoot Spread
+        Vector3 shootDir = FireEndPoint.forward + new Vector3(
+        Random.Range(
+            -shootConfigOS.Spread.x,
+            shootConfigOS.Spread.x
+        ),
+        Random.Range(
+            -shootConfigOS.Spread.y,
+            shootConfigOS.Spread.y
+        ),
+        Random.Range(
+            -shootConfigOS.Spread.z,
+             shootConfigOS.Spread.z
+        ));
+        shootDir.Normalize();
+
+        float _moveSpeed = 90f;
+        rigidbody.AddForce(shootDir * _moveSpeed, ForceMode.Impulse);
 
         float timeDestroySelf = 1f;
         Destroy(gameObject, timeDestroySelf);
--- Assets/Scripts/SO/ShootConfigSO.cs
+++ Assets/Scripts/SO/ShootConfigSO.cs
@@ -5,4 +5,5 @@ public class ShootConfigSO : ScriptableObject
 {
     public float Damage;
     public float FireRate;
+    public Vector3 Spread;
 }
